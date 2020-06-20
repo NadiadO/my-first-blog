2b042cc15cd1d2861e232dff8787ecd535e2e49b
nadia <nadiad68@hotmail.com>
2020-06-20 20:09:28 +0100

    Fixed an error with the newly added comment feature.

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,9 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
     author = models.CharField(max_length=200)
@@ -27,9 +30,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-    
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.text

7007467b004f9ca5fa661f8b29bce55dcf17d007
nadia <nadiad68@hotmail.com>
2020-06-20 20:44:21 +0100

    Commented out code for the comments feature.

--- blog/models.py
+++ blog/models.py
@@ -14,11 +14,11 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
-    def __str__(self):
-        return self.title
-
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
